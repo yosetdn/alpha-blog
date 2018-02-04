7cfd7d0d3224ff675ea5d92f0edb7b8f2bd035c3
Yoset Grajeda <yosetdn@gmail.com>
2018-02-03 21:19:00 -0600

    Adding signup page

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root 'welcome#home'
   get 'welcome/about',to:'welcome#about'
   resources :articles
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
 end
