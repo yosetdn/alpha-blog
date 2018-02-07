f455e3c7b936596e87c3715283c5e6ca70f790a5
Yoset Grajeda <yosetdn@gmail.com>
2018-02-06 21:33:22 -0600

    Fix article creator

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,8 +12,7 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = Article.new(article_params)
-    @article.user = User.first
+    @article.user = current_user
     if @article.save
       flash[:success] = "Article was successfully created"
       redirect_to article_path(@article)
